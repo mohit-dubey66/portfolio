816380814fa085c4f025202de487e5c1034cdeda
Mohit Dubey <mohitdubey1024@gmail.com>
2022-04-02 16:41:57 +0530

    done settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-n&t25nu#%*3o$woc7&z(v3^94zsrb+43rv6m5r_9kmsh(j%*p0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mohitdubey.herokuapp.com','localhost', '127.0.0.1:8000/']
 
 
 # Application definition
